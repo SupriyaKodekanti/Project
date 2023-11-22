014d45446158296a5360ca3fd7917e08f40bbe21
SupriyaKodekanti <144899172+SupriyaKodekanti@users.noreply.github.com>
2023-11-22 16:22:35 +0530

    Add files via upload
    commit2

---
+++ PhramaEasy/src/main/java/com/infinite/model/AdminLogin.java
@@ -0,0 +1,38 @@
+package com.infinite.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "admin")
+public class AdminLogin {
+	@Id
+	@Column(name = "User_name")
+	private String username;
+
+	@Column(name = "Password")
+	private String password;
+
+	public AdminLogin() {
+
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
